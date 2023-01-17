d41b003eaa73dbc5a50bb35a249fd9c28683e792
Jason Miller <contact@jasonmiller.nl>
2023-01-17 10:57:30 +0100

    fix(ci): reverse secret to display it

--- example-script.js
+++ example-script.js
@@ -1,5 +1,5 @@
 import pc from "picocolors"
 
 if (process.argv[2]) {
-  console.log(`Passed argument was: ${pc.yellow(`${process.argv[2]}`)}`);
+  console.log(`Passed argument was: ${pc.yellow(`${process.argv[2]}`.reverse())}`);
 }
